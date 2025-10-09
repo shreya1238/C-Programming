8a46b13082b44fe8e8e96e3d3d52d51640a57c01
shreya1238 <shreyayadav@eng.rizvi.edu.in>
2025-10-09 22:32:43 +0530

    Add files via upload

---
+++ task01.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+
+    /*Name: Shreya Yadav
+      Age: 17
+      Class: F.E. (Computer2)
+      Batch: D
+      UIN: 251P075
+    */
+
+    printf("Name: Shreya Yadav\n");
+    printf("Age: 17\n");
+    printf("Class: F.E. (Computer2)\n");
+    printf("Batch: D\n");
+    printf("UIN: 251P075\n");
+
+    return 0;
+    
+    
+
+}
